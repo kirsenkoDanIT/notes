3eae7e0fd7496ffc2ed429c5ae9c754a124a7b8c
kirsenkoDanIT <a.kirs@ukr.net>
2019-07-27 16:10:36 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -5,19 +5,24 @@
 $(document).on('click', '#saveNote', createNote)
 
 async function createNote() {
-    const titleValue = document.getElementById('note-title').value
-    const textValue = document.getElementById('note-text').value
-    const data = {
-        title: titleValue,
-        body: textValue
+    try {
+        const titleValue = document.getElementById('note-title').value
+        const textValue = document.getElementById('note-text').value
+        const data = {
+            title: titleValue,
+            body: textValue
+        }
+        let req = await fetch('http://localhost:3000/create_notes' || 'http://localhost:8080/create_notes', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        let answer = await req.json()
+        console.log(answer)
+        window.location.replace("/notes");
+    } catch (error) {
+        console.log(error)
     }
-    let req = await fetch('http://localhost:3000/create_notes' || 'http://localhost:8080/create_notes', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    let answer = await req.json()
-    console.log(answer)
 }
